821d609de33383aec86489d654193ffeb7b311a6
Lajgit <3243570032@qq.com>
2026-07-03 11:52:28 +0800

    移除武松Flash模块未使用函数

--- Bootloader_wusong/App/Src/app_boot_flash.c
+++ Bootloader_wusong/App/Src/app_boot_flash.c
@@ -8,22 +8,6 @@ static void Flash_ClearFlags(void)
                            FLASH_FLAG_PGAERR | FLASH_FLAG_PGPERR | FLASH_FLAG_PGSERR);
 }
 
-static uint32_t Flash_GetSector(uint32_t address)
-{
-    if (address < 0x08004000U) return FLASH_SECTOR_0;
-    if (address < 0x08008000U) return FLASH_SECTOR_1;
-    if (address < 0x0800C000U) return FLASH_SECTOR_2;
-    if (address < 0x08010000U) return FLASH_SECTOR_3;
-    if (address < 0x08020000U) return FLASH_SECTOR_4;
-    if (address < 0x08040000U) return FLASH_SECTOR_5;
-    if (address < 0x08060000U) return FLASH_SECTOR_6;
-    if (address < 0x08080000U) return FLASH_SECTOR_7;
-    if (address < 0x080A0000U) return FLASH_SECTOR_8;
-    if (address < 0x080C0000U) return FLASH_SECTOR_9;
-    if (address < 0x080E0000U) return FLASH_SECTOR_10;
-    return FLASH_SECTOR_11;
-}
-
 static HAL_StatusTypeDef Flash_WriteUnlocked(uint32_t start_address,
                                              const uint8_t *data,
                                              uint32_t size)
