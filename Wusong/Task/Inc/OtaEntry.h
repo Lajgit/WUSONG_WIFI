a795b301c24ab65a5ec60e984fd32d1d6498471b
Lajgit <3243570032@qq.com>
2026-07-03 12:01:56 +0800

    补充武松APP升级请求接口

--- Wusong/Task/Inc/OtaEntry.h
+++ Wusong/Task/Inc/OtaEntry.h
@@ -2,7 +2,9 @@
 #define __OTA_ENTRY_H__
 
 #include "stdint.h"
+#include "stdbool.h"
 
+bool OTA_SetBootRequest(void);
 void OTA_EnterBootloader(void);
 void OTA_EntryReceive(void *self, void *mesg, uint8_t mesg_len);
 
