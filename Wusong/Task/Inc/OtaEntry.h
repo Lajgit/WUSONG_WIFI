2ea6924c23817ca0f215274c64bf909152cf2c49
Lajgit <3243570032@qq.com>
2026-07-03 13:44:36 +0800

    移除独立升级入口接收接口

--- Wusong/Task/Inc/OtaEntry.h
+++ Wusong/Task/Inc/OtaEntry.h
@@ -1,11 +1,9 @@
 #ifndef __OTA_ENTRY_H__
 #define __OTA_ENTRY_H__
 
-#include "stdint.h"
 #include "stdbool.h"
 
 bool OTA_SetBootRequest(void);
 void OTA_EnterBootloader(void);
-void OTA_EntryReceive(void *self, void *mesg, uint8_t mesg_len);
 
 #endif
